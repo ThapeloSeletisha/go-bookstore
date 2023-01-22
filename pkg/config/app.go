3220a80714b5fed7aa0a8f9fabae89f322ea6451
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2023-01-22 16:44:47 +0200

    added a comment about gorm.Open()

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -11,6 +11,8 @@ var (
 )
 
 func Connect() {
+	// the username, password and database name used for gorm.Open() is dependent on the local environment
+	// make your own database in your mysql server and edit the 3 variables here that are environment specific 
 	d, err := gorm.Open("mysql", "thapelo:thapelo@/go-bookstore?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
