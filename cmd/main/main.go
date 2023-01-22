7345a86f2ecd89ef69fd9dd7da53875beae45296
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2023-01-22 16:38:53 +0200

    completed tutorial

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql" // look up what the underscore is about
+	"github.com/ThapeloSeletisha/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:3000", r))
+}
